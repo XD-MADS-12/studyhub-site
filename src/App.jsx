7508cda648933b7e221899ec2fc7d7f54474dbe4
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2026-01-06 01:34:18 +0600

    Refactor App component structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -6,13 +6,14 @@ import Home from './pages/Home'
 import Notes from './pages/Notes'
 import Tools from './pages/Tools'
 import Blog from './pages/Blog'
+import './styles/global.css'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-900 text-gray-100 flex flex-col">
+      <div className="app-container">
         <Navbar />
-        <main className="flex-grow">
+        <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/notes" element={<Notes />} />
