f8262fc8fcca304473afd922825bd21de0888f53
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2026-01-06 01:17:26 +0600

    Set up React Router with main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,29 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Notes from './pages/Notes'
+import Tools from './pages/Tools'
+import Blog from './pages/Blog'
 
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-900 text-gray-100 flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/notes" element={<Notes />} />
+            <Route path="/tools" element={<Tools />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
